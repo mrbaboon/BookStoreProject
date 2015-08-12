b42b93a55a9916f31e8af41759b0c84af6d65af5
Tyson Battistella <tbattis@uvic.ca>
2015-08-11 18:10:13 -0700

    Added Django REST Framework

--- BookStore/urls.py
+++ BookStore/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^BookStoreApp/', include('BookStoreApp.urls', namespace="BookStoreApp")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'api-auth/', include('rest_framework.urls', namespace="rest_framework"))
 ]
