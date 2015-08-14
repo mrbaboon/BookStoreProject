af133393cc44b0552c3bcf4e22ab1b6ff5baaf4a
Tyson Battistella <tbattis@uvic.ca>
2015-08-14 13:58:13 -0700

    Added BookReview to Book Admin page

--- BookStoreApp/admin.py
+++ BookStoreApp/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import Author, Book
+from .models import Author, Book, BookReview
+
+class BookReviewInline(admin.StackedInline):
+    model = BookReview
+
+class BookAdmin(admin.ModelAdmin):
+    inlines = [BookReviewInline]
 
 class BookInline(admin.StackedInline):
     model = Book
@@ -9,4 +15,4 @@ class AuthorAdmin(admin.ModelAdmin):
     inlines = [BookInline]
 
 admin.site.register(Author, AuthorAdmin)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
