3889daf83ce1ef1e9bdd33b675b49a39f1967ecf
Tyson Battistella <tbattis@uvic.ca>
2015-08-14 14:21:50 -0700

    Author_detail template started

--- BookStoreApp/urls.py
+++ BookStoreApp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^book/(?P<book_id>[0-9]+)/$', views.book_detail, name='bookdetail'),
     url(r'^author/(?P<author_id>[0-9]+)/$', views.author_detail, name='authordetail'),
+#    url(r'^book/(?P<book_id>[0-9]+)/review/$', views.book_review, name='bookreview'),
 ]
--- BookStoreApp/views.py
+++ BookStoreApp/views.py
@@ -12,12 +12,14 @@ def index(request):
 
 def book_detail(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
-    print("book id is " + book_id)
-    print("book is " + book.book_title)
     context = {'book': book}
     return render(request, 'BookStoreApp/book_detail.html', context)
 
 def author_detail(request, author_id):
     author = get_object_or_404(Author, pk=author_id)
-    return HttpResponse("You're looking at %s." % author)
+    context = {'author': author}
+    return render(request, 'BookStoreApp/author_detail.html', context)
+
+#def book_review(request, bookreview_id):
+#    return render()
 
